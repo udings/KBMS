56483deb128b756f79dec797743ee63f9c385814
udins udins <theblackhole1410@gmail.com>
2025-05-15 13:41:07 +0700

    push semua yang baru dah

--- root/server/models/Item.js
+++ root/server/models/Item.js
@@ -1,39 +1,39 @@
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const itemSchema = new mongoose.Schema({
-  nama_aset: { type: String, required: true },
-  kategori: { type: String, required: true },
-  lokasi: { type: String, required: true },
-  tahun_perolehan: {
-    type: Number,
-    required: true,
-    min: 1900, // Opsional: batas minimum tahun
-    max: new Date().getFullYear() // Opsional: tidak boleh lebih dari tahun sekarang
-  },
-  sumber_perolehan: { type: String, required: true },
-  kondisi: {
-    type: String,
-    enum: ['Baik', 'Buruk', 'Rusak'],
-    required: true
-  },
-  kelayakan: {
-    type: String,
-    enum: ['Layak', 'Tidak Layak'],
-    required: true
-  },
-  jumlah_unit: {
-    type: Number,
-    required: true,
-    min: 1 
-  },
-  penanggung_jawab: { type: String, required: true },
-  status: {
-    type: String,
-    enum: ['Disimpan', 'Dipinjam', 'Dibuanh', 'Hilang'],
-    default: 'Disimpan'
-  },
-  keterangan: { type: String },
-  image: { type: String },
-}, { timestamps: true });
+  const itemSchema = new mongoose.Schema({
+    nama_aset: { type: String, required: true },
+    kategori: { type: String, required: true },
+    lokasi: { type: String, required: true },
+    tahun_perolehan: {
+      type: Number,
+      required: true,
+      min: 1900, // Opsional: batas minimum tahun
+      max: new Date().getFullYear() // Opsional: tidak boleh lebih dari tahun sekarang
+    },
+    sumber_perolehan: { type: String, required: true },
+    kondisi: {
+      type: String,
+      enum: ['Baik', 'Buruk', 'Rusak'],
+      required: true
+    },
+    kelayakan: {
+      type: String,
+      enum: ['Layak', 'Tidak Layak'],
+      required: true
+    },
+    jumlah_unit: {
+      type: Number,
+      required: true,
+      min: 1 
+    },
+    penanggung_jawab: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ['Disimpan', 'Dipinjam', 'Dibuanh', 'Hilang'],
+      default: 'Disimpan'
+    },
+    keterangan: { type: String },
+    image: { type: String },
+  }, { timestamps: true });
 
-module.exports = mongoose.model("Item", itemSchema);
+  module.exports = mongoose.model("Item", itemSchema);
