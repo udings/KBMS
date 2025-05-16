af5f70a6ceb53a986945ddcbca1341f213f76414
udins udins <theblackhole1410@gmail.com>
2025-05-16 13:45:53 +0700

    allowedOrigins

--- root/server/server.js
+++ root/server/server.js
@@ -12,8 +12,8 @@ app.use(express.json()); // Penting untuk parsing JSON dari body request
 const allowedOrigins = [
   "https://kbms-chi.vercel.app",
   "https://inventaris-hksyoy0d5-udinss-projects.vercel.app",
-  "https://kbms-53oqofyqm-udinss-projects.vercel.app",
-
+  "https://kbms-53oqofyqm-udinss-projects.vercel.app" // tambahkan https:// !!!
+];
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -24,6 +24,7 @@ app.use(cors({
     }
   }
 }));
+
 
 // ===== JWT Authentication Middleware =====
 function verifyToken(req, res, next) {
