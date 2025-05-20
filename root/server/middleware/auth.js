5fbb57b898019037b34068d82f8f97013126d4df
udins udins <theblackhole1410@gmail.com>
2025-05-20 11:35:21 +0700

    Commit semuaaaaaa

--- root/server/middleware/auth.js
+++ root/server/middleware/auth.js
@@ -5,11 +5,12 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
   if (!token) return res.status(401).json({ message: 'Login required' });
-  console.log("🧠 Decoded user from token:", req.user);
+  
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // { id, username, role }
+    console.log("🧠 Decoded user from token:", req.user);
     next();
   } catch (err) {
     return res.status(403).json({ message: 'Invalid or expired token' });
