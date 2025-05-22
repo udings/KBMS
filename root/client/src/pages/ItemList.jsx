a2c3ae3bc7898e0e9c42b6e9695e312f302f00ba
udins udins <theblackhole1410@gmail.com>
2025-05-22 15:46:28 +0700

    Bisa click gambar

--- root/client/src/pages/ItemList.jsx
+++ root/client/src/pages/ItemList.jsx
@@ -149,6 +149,16 @@ function ItemList() {
             {filteredItems.map((item) => (
               <tr key={item._id}>
                 <td className="py-2 px-4 text-center">
+                  {item.image ? (
+                    <img
+                      src={item.image}
+                      alt={item.nama_aset}
+                      onClick={() => setSelectedImage(item.image)}
+                      className="w-16 h-16 object-cover rounded-lg mx-auto cursor-pointer hover:scale-105 transition"
+                    />
+                    ) : (
+                        <p>No Image</p>
+                  )}
                   <div className="flex justify-center gap-2">
                     {editingId === item._id ? (
                       <>
@@ -184,8 +194,12 @@ function ItemList() {
                   </td>
                 ))}
                 <td className="py-2 px-4 text-center">
-                  {item.image ? (
-                    <img src={item.image} alt={item.nama_aset} className="w-16 h-16 object-cover rounded-lg mx-auto" />
+                  {item.image && (
+                    <img
+                      src={item.image}
+                      alt={item.nama_aset}
+                      onClick={() => setSelectedImage(item.image)}
+                      className="w-full h-40 object-cover rounded mt-2 cursor-pointer hover:scale-105 transition"/>
                   ) : (
                     <p>No Image</p>
                   )}
@@ -274,6 +288,23 @@ function ItemList() {
           </div>
         ))}
       </div>
+      {selectedImage && (
+  <div
+    className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+    onClick={() => setSelectedImage(null)}
+  >
+    <div className="relative">
+      <img src={selectedImage} alt="Preview" className="max-w-full max-h-screen rounded-lg shadow-lg" />
+      <button
+        onClick={() => setSelectedImage(null)}
+        className="absolute top-2 right-2 bg-white text-black px-2 py-1 rounded-full shadow hover:bg-gray-200"
+      >
+        ✕
+      </button>
+    </div>
+  </div>
+)}
+
     </div>
   );
 }
